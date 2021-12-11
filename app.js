809aa228d2ab1defe37e59867342a7d4e9c426b8
JorgeLuisRupay <jorgeluis17rh17@gmail.com>
2021-12-10 20:59:18 -0500

    backendv1

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 
 app.use('/',require('./routes/bsale'));
 
-app.listen(5000,() => {
-    console.log('Server corriendo en el puerto 5000');
-});
 
+ 
+app.listen( process.env.PORT,() =>{
+    console.log('Servidor corriendo en puerto',process.env.PORT);
+});
